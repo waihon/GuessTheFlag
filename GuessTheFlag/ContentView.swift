74be17a7d62359c047452ac14fc0e037389b0614
Waihon Yew <yewwaihon@gmail.com>
2021-05-21 22:55:55 +0800

    Layout a text prompt and 3 buttons with flag images

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -14,8 +14,21 @@ struct ContentView: View {
   var correctAnswer = Int.random(in: 0...2)
   
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    VStack(spacing: 30) {
+      VStack {
+        Text("Tap the flag of")
+        Text(countries[correctAnswer])
+      }
+      
+      ForEach(0 ..< 3) { number in
+        Button(action: {
+          // flag was tapped
+        }) {
+          Image(self.countries[number])
+            .renderingMode(.original)
+        }
+      }
+    }
   }
 }
 

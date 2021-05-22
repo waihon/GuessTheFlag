626dc8725a4b88e1b321d2e8a1ced79596c2df76
Waihon Yew <yewwaihon@gmail.com>
2021-05-22 12:43:59 +0800

    Maximum 20 rounds

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -16,6 +16,8 @@ struct ContentView: View {
   @State private var scoreTitle = ""
   @State private var message = ""
   @State private var score = 0
+  @State private var rounds = 1
+  let maximumRounds = 20
 
   var body: some View {
     ZStack {
@@ -65,19 +67,31 @@ struct ContentView: View {
     if number == correctAnswer {
       scoreTitle = "Correct"
       score += 1
-      message = "Your score is \(score)"
+      if rounds < maximumRounds {
+        message = "Your current score is \(score)."
+      } else {
+        message = "Your final score is \(score)."
+      }
     } else {
       scoreTitle = "Wrong"
       score -= 1
-      message = "That's the flag of \(self.countries[number])"
+      message = "That's the flag of \(self.countries[number])."
+      if rounds == maximumRounds {
+        message += "\nYour final score is \(score)."
+      }
     }
     
+    rounds += 1
     showingScore = true
   }
   
   func askQuestion() {
     countries.shuffle()
     correctAnswer = Int.random(in: 0...2)
+    if rounds > maximumRounds {
+      rounds = 1
+      score = 0
+    }
   }
 }
 
